0674b3dee7f804e65fcf3512a42fd9dfea30cb6f
Gethin <gethin.p.rawlings@gmail.com>
2018-03-19 14:18:43 +0000

    fix small issue and add redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,19 @@ import React, { Component } from 'react';
 import ProgrammeSearch from "./components/ProgrammeSearch";
 import Login from "./components/Login";
 import Main  from "./components/Main";
-import Security from "./components/Security";
 import UserAdmin  from "./components/UserAdmin";
 import { Switch, Route } from 'react-router-dom'
 
-const isLoggedIn = false;
-
 class App extends Component {
-
   
   render() {
     return (
       <div  className="App-intro">       
         <Switch>
           <Route exact path ='/' component={Login}/>
-          <Route component={Security}>
-            <Route path ='/main' component={Main}/>
-            <Route path ='/UserAdmin' component={UserAdmin}/>
-            <Route path ='/ProgrammeSearch' component={ProgrammeSearch}/>
-          </Route>
+          <Route path ='/main' component={Main}/>
+          <Route path ='/UserAdmin' component={UserAdmin}/>
+          <Route path ='/ProgrammeSearch' component={ProgrammeSearch}/>
         </Switch>          
       </div>
     );
