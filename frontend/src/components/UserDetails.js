afb55edba4bd3f23bde56e7fd89009d70724771d
Gethin Rawlings <gethin.p.rawlings@gmail.com>
2018-08-01 06:32:17 +0100

    update npm

--- frontend/src/components/UserDetails.js
+++ frontend/src/components/UserDetails.js
@@ -73,7 +73,7 @@ class UserDetails extends React.Component {
             <form id="userDetails" className="userDetails" onSubmit={this.handleSubmit}>
 
               <label for="username" className="userName">User Name</label>
-              <input type="text" name="userName" form="userDetails" value={userName} readonly />
+              <input type="text" name="userName" form="userDetails" value={userName} />
               <label for="userFirstname" className="userFirstname">First Name</label>
               <input type="text" name="userFirstname" form="userDetails" value={this.state.userFirstname} onChange={this.handleChange} />
               <label for="userLastname" className="userLastname">Last Name</label>
@@ -83,13 +83,13 @@ class UserDetails extends React.Component {
               <label for="userMobileNumber" className="userMobileNumber">Mobile mumber</label>
               <input type="text" name="userMobileNumber" form="userDetails" value={this.state.userMobileNumber} onChange={this.handleChange} />
               <label for="accountCreationDate" className="accountCreationDate">Account created on</label>
-              <input type="text" form="userDetails" name="accountCreationDate" value={accountCreationDate} readonly />
+              <input type="text" form="userDetails" name="accountCreationDate" value={accountCreationDate} />
               <label for="accountExpiryDate" className="accountExpiryDate">Account expired on</label>
               <input type="text" form="userDetails" name="accountExpiryDate" value={this.state.accountExpiryDate} onChange={this.handleChange} />
               <label for="userPassword" className="userPassword">Password</label>
               <input type="password" name="userPassword" form="userDetails" value={this.state.userPassword} onChange={this.handleChange} />
               <label for="passwordLastChanged" className="passwordLastChanged">Password changed</label>
-              <input type="text" form="userDetails" name="passwordLastChanged" value={passwordLastChanged} readonly />
+              <input type="text" form="userDetails" name="passwordLastChanged" value={passwordLastChanged} />
               <label for="userIdIndie" className="userIdIndie">Indie</label>
               <input type="text" name="userIdIndie" form="userDetails" value={this.state.userIdIndie} onChange={this.handleChange} />
               <button id="loginbutton" className="loginbutton">Save</button>
