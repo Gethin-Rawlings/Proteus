aab9421ba44ff02e708ef0d0cfa97757db427014
Gethin Rawlings <gethin.p.rawlings@gmail.com>
2018-08-10 07:38:06 +0100

    new function verifyTokens

--- backend/src/searches/programmesearch.js
+++ backend/src/searches/programmesearch.js
@@ -3,17 +3,21 @@ const router = express.Router();
 const formidable = require('express-formidable');
 const jwt = require('jsonwebtoken');
 const sql = require('../config/db')
+const verifyToken = require('../security/verifyToken')
 
 router.use(formidable());
 
 router.post('/programmesearch', async (req, res) =>{
-    const token = req.headers.authorization;
+    verifyToken(req)
+    /*const token = req.headers.authorization;
     jwt.verify(token, 'keyboard cat 4 ever', function(err, decoded) {
         if(err){
             console.log(err)
         }
+        req.userName = decoded.username
         console.log(decoded.username) 
-      });
+      });*/
+    console.log(req.userId)
     const request = new sql.Request();
     request.input('production', sql.Int, req.fields.production);
     request.input('network', sql.Int, req.fields.network);
---
+++ backend/src/security/verifyToken.js
@@ -0,0 +1,14 @@
+
+const jwt = require('jsonwebtoken');
+function verifyToken(req) {
+  const token = req.headers.authorization;
+  if (!token)
+    return res.status(403).send({ auth: false, message: 'No token provided.' });
+  jwt.verify(token, 'keyboard cat 4 ever', function(err, decoded) {
+    if (err)
+    return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    // if everything good, save to request for use in other routes
+    req.userId = decoded.username;
+  });
+}
+module.exports = verifyToken;
