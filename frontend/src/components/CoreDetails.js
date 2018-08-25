6658c72c8781d8f35dda04dc2facaf38fd092d70
Gethin Rawlings <gethin.p.rawlings@gmail.com>
2018-08-25 09:35:06 +0100

    layout

--- frontend/src/components/CoreDetails.js
+++ frontend/src/components/CoreDetails.js
@@ -1,17 +1,47 @@
 import React from 'react';
-//import "./details.css";
+import "./details.css";
 import "./Main.css"
 
 
 class CoreDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      programme: '',
+      history: this.history
+    };
+    this.handleChange = this
+      .handleChange
+      .bind(this);
+  /*  this.handleSubmit = this
+      .handleSubmit
+      .bind(this); */
+  }
+  handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    this.setState({[name]: value});
+  }
   
   render() {
     
     return (
       <div  onChange={this.handleChange} >
-        <section className='details'>
-            Something to go here for core details
+        <section >
+        <form onSubmit={this.handleSubmit} id='form' className='details'>
+            <section>
+              TX title <input name="programmeTitle" className="programmeTitle" type="text" form="form" placeholder="programmeTitle" value={this.state.programmeTitle} onChange={this.handleChange} />
+            </section>
+            <section>
+              Tx Details <input name="episodeTitle" className="episodeTitle" type="text" form="form" placeholder="episodeTitle" value={this.state.episodeTitle} onChange={this.handleChange} />
+            </section>
+            <section>
+              Tx Type <input name="commissionTitle" className="commissionTitle" type="text" form="form" placeholder="commissionTitle" value={this.state.commissionTitle} onChange={this.handleChange} />
+            </section>
+            </form>
         </section>
+       
       </div>
     );
   }
