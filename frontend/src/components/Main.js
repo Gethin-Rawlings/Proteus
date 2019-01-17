937cd32c5e309ae67698a0976ee89f5388c2103e
Gethin Rawlings <gethin.p.rawlings@gmail.com>
2019-01-17 07:47:56 +0000

    start workon main page

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -9,18 +9,21 @@ class Main extends React.Component {
   render() {
     return (
       <div className="main">
-        
         <Navbar/>
         <section className="App-intro">
         <section className="pad">
-          Grumpy cat was ere again
-        <Brands />
+          <section> 
+            Welcome to the new version of Proteus.
+          </section>
+          <section>
+          Please ensure you always access Proteus with one of the recommended browsers:  
+            Firefox, Microsoft Edge, Safari or Google Chrome.
+          </section>
         </section>
         </section>
         <Footer/>
       </div>
-
-    );
+    )
   }
 }
 export default Main;
